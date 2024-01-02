8ad5b277be71d116016ef3d1eaaa453c89b3b227
Andrew <andrewstaveley14@gmail.com>
2024-01-02 18:22:08 -0500

    Added functionality to allow for the display of mountain names

--- src/pages/Maine.js
+++ src/pages/Maine.js
@@ -1,10 +1,26 @@
 import NavBar from "../components/NavBar.js"
+import {useState, useEffect} from "react"
+import MountainCard from "../components/MountainCard.js"
+
 function Maine() {
+    const [mountains, setMountains] = useState([])
+    
+    useEffect(() => {
+        fetch("http://localhost:3000/mountains")
+        .then ((r) => r.json())
+        .then((data) => setMountains(data))
+    }, [])
+
+    let maineMountains = mountains.filter((mountain) => mountain.state === "Maine")
+
     return (
         <>
             <header>
                 {<NavBar />}
             </header>
+            <main>
+                {maineMountains.map((mountain) => <MountainCard key={mountain.id} mountain={mountain}/>)}
+            </main>
         </>
     )
 }
