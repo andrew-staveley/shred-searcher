7b478c510f527906ca4c380c27a7ad4eb44e6592
Andrew <andrewstaveley14@gmail.com>
2024-01-02 15:24:35 -0500

    Initial commit for routes, added route information

---
+++ src/routes.js
@@ -0,0 +1,36 @@
+import Home from "./pages/Home.js"
+import Maine from "./pages/Maine.js"
+import NewHampshire from "./pages/NewHampshire.js"
+import Vermont from "./pages/Vermont.js"
+import Mountain from "./pages/Mountain.js"
+import ErrorPage from "./pages/ErrorPage.js"
+
+const routes = [
+    {
+        path: "/",
+        element: <Home />,
+        errorElement: <ErrorPage />
+    },
+    {
+        path: "/maine",
+        element: <Maine />,
+        errorElement: <ErrorPage />
+    },
+    {
+        path: "/newhampshire",
+        element: <NewHampshire />,
+        errorElement: <ErrorPage />
+    },
+    {
+        path: "/vermont",
+        element: <Vermont />,
+        errorElement: <ErrorPage />
+    },
+    {
+        path: "/mountain/:id",
+        element: <Mountain />,
+        errorElement: <ErrorPage />
+    }
+];
+
+export default routes
