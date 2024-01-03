7fc4382a0d09a22e39c935acd138f29d0a591645
Andrew <andrewstaveley14@gmail.com>
2024-01-03 17:03:21 -0500

    Imported CSS files to allow for styling

--- src/components/MountainCard.js
+++ src/components/MountainCard.js
@@ -1,9 +1,11 @@
 import {Link} from 'react-router-dom'
+import "../css/MountainCard.css"
+
 function MountainCard({mountain}) {
     return (
-        <article>
+        <article className="mountain">
             <h2>{mountain.name}</h2>
-            {<Link to={`/mountain/${mountain.id}`}>More Information</Link>}
+            {<Link to={`/mountain/${mountain.id}`}>Explore {mountain.name}</Link>}
         </article>
     )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,10 @@
 import {NavLink } from "react-router-dom"
-import "./NavBar.css"
+import "../css/NavBar.css"
 
 function NavBar() {
     return (
         <nav className="navbar">
+            <h1 className="titlenavbar">Shred Searcher - The Best Place to Explore Mountains</h1>
             <NavLink to="/">Home</NavLink>
             <NavLink to="/maine">Maine</NavLink>
             <NavLink to="/newhampshire">New Hampshire</NavLink>
