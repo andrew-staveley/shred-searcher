69f03434e96bfb6c0845f20fed7721bff75fe117
Andrew <andrewstaveley14@gmail.com>
2024-01-02 19:09:52 -0500

    Added link to mountain-specific page

--- src/components/MountainCard.js
+++ src/components/MountainCard.js
@@ -1,7 +1,9 @@
+import {Link} from 'react-router-dom'
 function MountainCard({mountain}) {
     return (
         <article>
             <h2>{mountain.name}</h2>
+            {<Link to={`/mountain/${mountain.id}`}>More Information</Link>}
         </article>
     )
 }
