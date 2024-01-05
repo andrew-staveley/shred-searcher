7b023916d987be4775a7c45bde6d203157e44465
Andrew <andrewstaveley14@gmail.com>
2024-01-05 11:56:32 -0500

    Added content to error page, allows for a redirect to the homepage

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,10 +1,16 @@
 import NavBar from "../components/NavBar.js"
+import {Link} from "react-router-dom"
+
 function ErrorPage() {
     return (
         <>
             <header>
                 {<NavBar />}
             </header>
+            <main>
+                <h1>Looks like we've encountered a problem.</h1>
+                <Link to="/"><button>Return Home</button></Link>
+            </main>
         </>
     )
 }
